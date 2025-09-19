f7ec45a323ce71412d6a49627a26763ef2b634c1
Andrew Durbin <andrewd@zededa.com>
2025-09-19 14:23:24 -0600

    Generated files for cluster-types
    make proto-container
    
    Signed-off-by: Andrew Durbin <andrewd@zededa.com>

--- python/config/edge_node_cluster_pb2.py
+++ python/config/edge_node_cluster_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 from evecommon import acipherinfo_pb2 as evecommon_dot_acipherinfo__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1e\x63onfig/edge_node_cluster.proto\x12\x15org.lfedge.eve.config\x1a\x1b\x65vecommon/acipherinfo.proto\"\xe6\x01\n\x0f\x45\x64geNodeCluster\x12\x14\n\x0c\x63luster_name\x18\x01 \x01(\t\x12\x12\n\ncluster_id\x18\x02 \x01(\t\x12\x19\n\x11\x63luster_interface\x18\x03 \x01(\t\x12\x19\n\x11\x63luster_ip_prefix\x18\x04 \x01(\t\x12\x16\n\x0eis_worker_node\x18\x05 \x01(\x08\x12\x16\n\x0ejoin_server_ip\x18\x06 \x01(\t\x12\x43\n\x17\x65ncrypted_cluster_token\x18\x07 \x01(\x0b\x32\".org.lfedge.eve.common.CipherBlockB=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve-api/go/configb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1e\x63onfig/edge_node_cluster.proto\x12\x15org.lfedge.eve.config\x1a\x1b\x65vecommon/acipherinfo.proto\"\xbd\x02\n\x0f\x45\x64geNodeCluster\x12\x14\n\x0c\x63luster_name\x18\x01 \x01(\t\x12\x12\n\ncluster_id\x18\x02 \x01(\t\x12\x19\n\x11\x63luster_interface\x18\x03 \x01(\t\x12\x19\n\x11\x63luster_ip_prefix\x18\x04 \x01(\t\x12\x16\n\x0eis_worker_node\x18\x05 \x01(\x08\x12\x16\n\x0ejoin_server_ip\x18\x06 \x01(\t\x12\x43\n\x17\x65ncrypted_cluster_token\x18\x07 \x01(\x0b\x32\".org.lfedge.eve.common.CipherBlock\x12\x38\n\x0c\x63luster_type\x18\x08 \x01(\x0e\x32\".org.lfedge.eve.config.ClusterType\x12\x1b\n\x13tie_breaker_node_id\x18\t \x01(\t*\x80\x01\n\x0b\x43lusterType\x12\x1c\n\x18\x43LUSTER_TYPE_UNSPECIFIED\x10\x00\x12\x19\n\x15\x43LUSTER_TYPE_K3S_BASE\x10\x01\x12#\n\x1f\x43LUSTER_TYPE_REPLICATED_STORAGE\x10\x02\x12\x13\n\x0f\x43LUSTER_TYPE_HA\x10\x03\x42=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve-api/go/configb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -23,6 +23,8 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'\n\025org.lfedge.eve.configZ$github.com/lf-edge/eve-api/go/config'
+  _globals['_CLUSTERTYPE']._serialized_start=407
+  _globals['_CLUSTERTYPE']._serialized_end=535
   _globals['_EDGENODECLUSTER']._serialized_start=87
-  _globals['_EDGENODECLUSTER']._serialized_end=317
+  _globals['_EDGENODECLUSTER']._serialized_end=404
 # @@protoc_insertion_point(module_scope)
