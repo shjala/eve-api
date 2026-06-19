186e61c68f39cc54a87593b11d26ccfa27a594ea
naiming-zededa <naiming@zededa.com>
2026-06-19 10:03:09 -0700

    generated files: config: add enable_native_k8s_orchestration to EdgeNodeCluster
    - add generated files.
    
    Signed-off-by: naiming-zededa <naiming@zededa.com>

--- python/config/edge_node_cluster_pb2.py
+++ python/config/edge_node_cluster_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 from evecommon import acipherinfo_pb2 as evecommon_dot_acipherinfo__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1e\x63onfig/edge_node_cluster.proto\x12\x15org.lfedge.eve.config\x1a\x1b\x65vecommon/acipherinfo.proto\"F\n\x15LoadBalancerInterface\x12\x16\n\x0einterface_name\x18\x01 \x01(\t\x12\x15\n\raddress_cidrs\x18\x02 \x03(\t\"W\n\x13LoadBalancerService\x12@\n\ninterfaces\x18\x01 \x03(\x0b\x32,.org.lfedge.eve.config.LoadBalancerInterface\"\xd7\x03\n\x0f\x45\x64geNodeCluster\x12\x14\n\x0c\x63luster_name\x18\x01 \x01(\t\x12\x12\n\ncluster_id\x18\x02 \x01(\t\x12\x19\n\x11\x63luster_interface\x18\x03 \x01(\t\x12\x19\n\x11\x63luster_ip_prefix\x18\x04 \x01(\t\x12\x16\n\x0eis_worker_node\x18\x05 \x01(\x08\x12\x16\n\x0ejoin_server_ip\x18\x06 \x01(\t\x12\x43\n\x17\x65ncrypted_cluster_token\x18\x07 \x01(\x0b\x32\".org.lfedge.eve.common.CipherBlock\x12\x38\n\x0c\x63luster_type\x18\x08 \x01(\x0e\x32\".org.lfedge.eve.config.ClusterType\x12\x1b\n\x13tie_breaker_node_id\x18\t \x01(\t\x12\x32\n\tha_scheme\x18\x0b \x01(\x0e\x32\x1f.org.lfedge.eve.config.HAScheme\x12I\n\x15load_balancer_service\x18\x0c \x01(\x0b\x32*.org.lfedge.eve.config.LoadBalancerService\x12\x19\n\x11master_node_uuids\x18\r \x03(\t*\x84\x01\n\x0b\x43lusterType\x12\x1c\n\x18\x43LUSTER_TYPE_UNSPECIFIED\x10\x00\x12\x19\n\x15\x43LUSTER_TYPE_K3S_BASE\x10\x01\x12#\n\x1f\x43LUSTER_TYPE_REPLICATED_STORAGE\x10\x02\x12\x17\n\x0f\x43LUSTER_TYPE_HA\x10\x03\x1a\x02\x08\x01*A\n\x08HAScheme\x12\x19\n\x15HA_SCHEME_UNSPECIFIED\x10\x00\x12\x1a\n\x16HA_SCHEME_K3S_EMBEDDED\x10\x01\x42=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve-api/go/configb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1e\x63onfig/edge_node_cluster.proto\x12\x15org.lfedge.eve.config\x1a\x1b\x65vecommon/acipherinfo.proto\"F\n\x15LoadBalancerInterface\x12\x16\n\x0einterface_name\x18\x01 \x01(\t\x12\x15\n\raddress_cidrs\x18\x02 \x03(\t\"W\n\x13LoadBalancerService\x12@\n\ninterfaces\x18\x01 \x03(\x0b\x32,.org.lfedge.eve.config.LoadBalancerInterface\"\x80\x04\n\x0f\x45\x64geNodeCluster\x12\x14\n\x0c\x63luster_name\x18\x01 \x01(\t\x12\x12\n\ncluster_id\x18\x02 \x01(\t\x12\x19\n\x11\x63luster_interface\x18\x03 \x01(\t\x12\x19\n\x11\x63luster_ip_prefix\x18\x04 \x01(\t\x12\x16\n\x0eis_worker_node\x18\x05 \x01(\x08\x12\x16\n\x0ejoin_server_ip\x18\x06 \x01(\t\x12\x43\n\x17\x65ncrypted_cluster_token\x18\x07 \x01(\x0b\x32\".org.lfedge.eve.common.CipherBlock\x12\x38\n\x0c\x63luster_type\x18\x08 \x01(\x0e\x32\".org.lfedge.eve.config.ClusterType\x12\x1b\n\x13tie_breaker_node_id\x18\t \x01(\t\x12\x32\n\tha_scheme\x18\x0b \x01(\x0e\x32\x1f.org.lfedge.eve.config.HAScheme\x12I\n\x15load_balancer_service\x18\x0c \x01(\x0b\x32*.org.lfedge.eve.config.LoadBalancerService\x12\x19\n\x11master_node_uuids\x18\r \x03(\t\x12\'\n\x1f\x65nable_native_k8s_orchestration\x18\x0e \x01(\x08*\x84\x01\n\x0b\x43lusterType\x12\x1c\n\x18\x43LUSTER_TYPE_UNSPECIFIED\x10\x00\x12\x19\n\x15\x43LUSTER_TYPE_K3S_BASE\x10\x01\x12#\n\x1f\x43LUSTER_TYPE_REPLICATED_STORAGE\x10\x02\x12\x17\n\x0f\x43LUSTER_TYPE_HA\x10\x03\x1a\x02\x08\x01*A\n\x08HAScheme\x12\x19\n\x15HA_SCHEME_UNSPECIFIED\x10\x00\x12\x1a\n\x16HA_SCHEME_K3S_EMBEDDED\x10\x01\x42=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve-api/go/configb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -25,14 +25,14 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._serialized_options = b'\n\025org.lfedge.eve.configZ$github.com/lf-edge/eve-api/go/config'
   _CLUSTERTYPE.values_by_name["CLUSTER_TYPE_HA"]._options = None
   _CLUSTERTYPE.values_by_name["CLUSTER_TYPE_HA"]._serialized_options = b'\010\001'
-  _globals['_CLUSTERTYPE']._serialized_start=722
-  _globals['_CLUSTERTYPE']._serialized_end=854
-  _globals['_HASCHEME']._serialized_start=856
-  _globals['_HASCHEME']._serialized_end=921
+  _globals['_CLUSTERTYPE']._serialized_start=763
+  _globals['_CLUSTERTYPE']._serialized_end=895
+  _globals['_HASCHEME']._serialized_start=897
+  _globals['_HASCHEME']._serialized_end=962
   _globals['_LOADBALANCERINTERFACE']._serialized_start=86
   _globals['_LOADBALANCERINTERFACE']._serialized_end=156
   _globals['_LOADBALANCERSERVICE']._serialized_start=158
   _globals['_LOADBALANCERSERVICE']._serialized_end=245
   _globals['_EDGENODECLUSTER']._serialized_start=248
-  _globals['_EDGENODECLUSTER']._serialized_end=719
+  _globals['_EDGENODECLUSTER']._serialized_end=760
 # @@protoc_insertion_point(module_scope)
